94e34130ee14b78278c8f7028a800509ad162011
Chayanin <arm.crazyplay@gmail.com>
2023-07-19 09:35:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lab 09 : Todo List
+
+After cloning repository, open the project with VSCode and run the following commands in terminal:
+
+```bash
+npm install
+npm run dev
+```
+
+ให้นักศึกษาสร้างหน้าเว็บ frontend ที่แสดงหน้า Mockup (จำลอง) ของ Todo List App โดยใช้ความรู้เรื่อง React และการแบ่ง Component แบบพื้นฐาน
+
+[คลิกเพื่อดูรายละเอียด]()
+
+หลังจาก Deploy ด้วย Vercel แล้ว ให้แปะลิงค์ Deploy และลิงค์ GitHub Repo ของน.ศ. ไว้ใน Spreadsheet ด้านล่าง
+
+[คลิกเพื่อเปิด Spreadsheet]()
---
+++ src/components/Task.jsx
@@ -0,0 +1,27 @@
+export const Task = ({
+  id,
+  title,
+  deleteTaskFunc,
+  toggleDoneTaskFunc,
+  completed,
+}) => {
+  const deleteBtnOnClick = () => {
+    deleteTaskFunc(id);
+  };
+
+  return (
+    <div className="d-flex p-3 gap-2 align-items-center border-bottom">
+      {/*
+      HINTS: if task is completed, below "span" will show like this 
+        <span className="text-decoration-line-through">{title}</span>
+        But if task is not completed : 
+        <span>{title}</span>
+      */}
+      <span>{title}</span>
+      <button className="btn btn-success">Done</button>
+      <button className="btn btn-danger" onClick={deleteBtnOnClick}>
+        Delete
+      </button>
+    </div>
+  );
+};
